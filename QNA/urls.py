9f231db8c33c06f27698354c72da74db0a68c870
Shamsuddin Sayed <syedshams316@gmail.com>
2020-07-28 18:07:44 +0530

    Add first view, base files

---
+++ QNA/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.question_list, name='question_list')
+]
--- QNA/views.py
+++ QNA/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
+
+
+def question_list(request):
+    questions = Question.objects.order_by('created')
+    return render(request, 'qna/question_list.html', {'questions': questions})
