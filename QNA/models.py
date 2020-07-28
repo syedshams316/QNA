371000cf4dfa9631ba4020fa881bdbf45137e557
Shamsuddin Sayed <syedshams316@gmail.com>
2020-07-28 17:20:45 +0530

    Initial commit

---
+++ QNA/models.py
@@ -0,0 +1,9 @@
+# from django.conf import settings
+# from django.db import models
+#
+#
+# # Create your models here.
+# class Question(models.Model):
+#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     text = models.CharField(max_length=200)
+#
