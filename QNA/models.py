32ffc18456441ff8bcc94f28823d1ae5d2dbe95a
Shamsuddin Sayed <syedshams316@gmail.com>
2020-07-28 17:40:24 +0530

    Admin registration, initial model

--- QNA/models.py
+++ QNA/models.py
@@ -1,9 +1,13 @@
-# from django.conf import settings
-# from django.db import models
-#
-#
-# # Create your models here.
-# class Question(models.Model):
-#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     text = models.CharField(max_length=200)
-#
+from django.conf import settings
+from django.db import models
+
+
+# Create your models here.
+class Question(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
