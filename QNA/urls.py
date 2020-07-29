311754b005aad1af37fabf2ae39e8e716d5a0c4e
Shamsuddin Sayed <syedshams316@gmail.com>
2020-07-29 13:32:18 +0530

    Add CURD funtionality to questions

--- QNA/urls.py
+++ QNA/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.question_list, name='question_list'),
+    path('', views.index, name='index'),
     path('<int:pk>/', views.all_answers, name='all_answers'),
     path('new/', views.create_question, name='new_question'),
+    path('edit/<int:pk>/', views.edit_question, name='edit_question'),
+    path('delete/<int:pk>/', views.delete_question, name='delete_question'),
 ]
--- QNA/views.py
+++ QNA/views.py
@@ -5,18 +5,12 @@ from .forms import QuestionForm
 # Create your views here.
 
 
-def question_list(request):
+def index(request):
     questions = Question.objects.order_by('created')
     return render(request, 'qna/question_list.html', {'questions': questions})
 
 
-def all_answers(request, pk):
-    question = get_object_or_404(Question, pk=pk)
-    answers = question.answers.all().order_by('created')
-    return render(request, 'qna/all_answers.html',
-                  {'question': question, 'answers': answers})
-
-
+# CRUD of question
 def create_question(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
@@ -28,3 +22,31 @@ def create_question(request):
     else:
         form = QuestionForm()
     return render(request, 'qna/new_question.html', {'form': form})
+
+
+def all_answers(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    answers = question.answers.all().order_by('created')
+    return render(request, 'qna/all_answers.html',
+                  {'question': question, 'answers': answers})
+
+
+def edit_question(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.save()
+            return redirect('question_list')
+    else:
+        form = QuestionForm(instance=question)
+    return render(request, 'qna/edit_question.html', {'form': form})
+
+
+def delete_question(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    question.delete()
+    return redirect('question_list')
+
