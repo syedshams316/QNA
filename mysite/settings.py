c3582fbc3b70d7fefb72c9b7198fee016592aa1c
Shamsuddin Sayed <syedshams316@gmail.com>
2020-08-14 13:14:49 +0530

    additional files and changes for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'QUORA',
+        'NAME': 'Quora',
         'USER': 'name',
         'PASSWORD': '',
         'HOST': 'localhost',
